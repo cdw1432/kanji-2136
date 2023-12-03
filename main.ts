e2ada5af5d0c80c6e62714d811bbaad84a4e5047
Woojin Choi <cdw6791432@gmail.com>
2023-12-02 16:41:59 -0800

    kanji list

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
-console.log('hello world')
+import {jsonData} from './src/data-2136.js';
+
+interface Character {
+  "New (Shinjitai)": string;
+}
+
+const firstChar: Character = jsonData[0];
+const lastChar: Character = jsonData[2136 - 1];
+
+let kanji = document.getElementById('kanji');
+if (kanji) {
+  kanji.innerHTML += `<h1>` + firstChar["New (Shinjitai)"]; + `</h1>`
+  kanji.innerHTML += `<h1>` + lastChar["New (Shinjitai)"]; + `</h1>`
+}
+
+console.log(firstChar);
+console.log(lastChar);
