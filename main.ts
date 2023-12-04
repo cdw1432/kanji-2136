6628964181eab4e4487536aeda11d204596431ca
Woojin Choi <cdw6791432@gmail.com>
2023-12-03 16:49:22 -0800

    display kanji

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 import {jsonData} from './src/data-2136.js';
 
+/* Global */
 let selectedMenu: number = -1;
+let jsonDataUpdated = jsonData;
+let mainContent = document.getElementById('main')
 /* MENU BUTTON */
 const menuButton = document.getElementById('menu-button');
 if (menuButton) {
@@ -32,6 +35,7 @@ optionArray.forEach((opt) => {
   buttonElement.addEventListener('click', () => {
     selectedMenu = (OPTIONS as any)[opt].toString();
     updateData()
+    showData()
   });
 
   optionElement.appendChild(buttonElement);
@@ -43,29 +47,30 @@ optionArray.forEach((opt) => {
 let updateData = () => {
   const grade:string = (selectedMenu == 7) ? 'S' : selectedMenu.toString();
   if(grade != "0")
-    console.log(jsonData.filter((e) => e.Grade == grade))
+    jsonDataUpdated = jsonData.filter((e) => e.Grade == grade)
   else
-    console.log(jsonData)
-
+    jsonDataUpdated = jsonData;
+}
+/* KANJI */
+interface Character {
+  "New (Shinjitai)": string,
+  "English meaning": string,
+  "Readings": string,
 }
 
-
-
-// interface Character {
-//   "New (Shinjitai)": string;
-// }
-
-// const firstChar: Character = jsonData[0];
-// const lastChar: Character = jsonData[2136 - 1];
-
-// let kanji = document.getElementById('kanji');
-// if (kanji) {
-//   kanji.innerHTML += `<h1>` + firstChar["New (Shinjitai)"]; + `</h1>`
-//   kanji.innerHTML += `<h1>` + lastChar["New (Shinjitai)"]; + `</h1>`
-// }
-
-// console.log(firstChar);
-// console.log(lastChar);
+let showData = () => {
+  const rChar:Character = jsonDataUpdated[Math.floor(Math.random() * jsonDataUpdated.length)]
+  const exist = mainContent?.querySelector(`#character`);
+  if(exist) {
+      exist.textContent = rChar["New (Shinjitai)"]
+  } else {
+    const charElement = document.createElement('h2');
+    charElement.textContent = rChar["New (Shinjitai)"][0];
+    charElement.id = "character"
+    mainContent?.appendChild(charElement)
+  }
+  console.log(rChar);
+}
 
 //right arrow
 //<svg xmlns="http://www.w3.org/2000/svg" height="16" width="10" viewBox="0 0 320 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path fill="#000000" d="M278.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-160 160c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L210.7 256 73.4 118.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l160 160z"/></svg>
